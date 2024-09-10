e320335e1af457435685aa2f4847c9c2f9504a3b
gopikrishna3581 <krishnabhukya321@gmail.com>
2024-09-10 19:50:15 +0530

    hello

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { useNavigate } from 'react-router-dom';
+
 
 const Home = () => {
+  const navigate = useNavigate()
+
+  const handleurl = () =>{
+    navigate("/projects");
+  }
   return (
-    <section className="relative bg-gray-900 text-gray-100 min-h-screen flex items-center justify-center overflow-hidden">
-      <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: 'url(https://via.placeholder.com/1500x800)' }}>
+    <section className="relative  text-gray-100 min-h-screen flex items-center justify-center overflow-hidden">
+      <div className="absolute inset-0 bg-cover bg-center" >
         <div className="absolute inset-0 bg-black opacity-50"></div>
       </div>
       <div className="relative z-10 text-center px-4 py-12 max-w-3xl mx-auto">
@@ -26,7 +33,7 @@ const Home = () => {
           Explore my projects and skills to learn more about what I can do.
         </motion.p>
         <a
-          href="/projects"
+          onClick={handleurl}
           className="inline-block px-8 py-4 bg-purple-600 text-gray-100 font-semibold rounded-lg shadow-lg transition-transform transform hover:scale-105"
         >
           Explore My Work
